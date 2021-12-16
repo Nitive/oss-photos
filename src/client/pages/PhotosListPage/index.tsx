09caf54533ff6698d406b366db009dc488a8b58a
“Rostislav <antonov.qwertylox004@yandex.ru>
2021-12-16 14:07:57 +0300

    add env to gitignore and create slider

--- src/client/pages/PhotosListPage/index.tsx
+++ src/client/pages/PhotosListPage/index.tsx
@@ -45,27 +45,14 @@ export default function PhotosListPage() {
               <span></span>
               <span></span>
             </button>
-            <button className={css.arrow_right}>
-              <svg
-                version="1.1"
-                id="Capa_1"
-                xmlns="http://www.w3.org/2000/svg"
-                x="0px"
-                y="0px"
-                viewBox="0 0 306 306"
-                style="enable-background:new 0 0 306 306"
-              >
-                <g>
-                  <g>
-                    <polygon
-                      points="58.65,267.75 175.95,153 58.65,35.7 94.35,0 247.35,153 94.35,306"
-                      fill="#b5b5b5"
-                    />
-                  </g>
-                </g>
-              </svg>
-            </button>
-            <button className={css.arrow_left}>
+            <button
+              className={css.arrow_left}
+              onClick={() => {
+                const nextPhoto =
+                  openPhoto.i === 0 ? photos.length - 1 : openPhoto.i - 1
+                setOpenPhoto({ i: nextPhoto })
+              }}
+            >
               <svg
                 version="1.1"
                 id="Capa_1"
@@ -86,7 +73,38 @@ export default function PhotosListPage() {
                 </g>
               </svg>
             </button>
-            <img className={css.open_photo} src={openPhoto.preview} alt="" />
+            <button
+              className={css.arrow_right}
+              onClick={() => {
+                const nextPhoto =
+                  openPhoto.i === photos.length - 1 ? 0 : openPhoto.i + 1
+                setOpenPhoto({ i: nextPhoto })
+              }}
+            >
+              <svg
+                version="1.1"
+                id="Capa_1"
+                xmlns="http://www.w3.org/2000/svg"
+                x="0px"
+                y="0px"
+                viewBox="0 0 306 306"
+                style="enable-background:new 0 0 306 306"
+              >
+                <g>
+                  <g>
+                    <polygon
+                      points="58.65,267.75 175.95,153 58.65,35.7 94.35,0 247.35,153 94.35,306"
+                      fill="#b5b5b5"
+                    />
+                  </g>
+                </g>
+              </svg>
+            </button>
+            <img
+              className={css.open_photo}
+              src={photos[openPhoto.i].preview}
+              alt=""
+            />
           </div>
         ) : (
           photos.map((photo: any, i: any) => {
@@ -95,7 +113,7 @@ export default function PhotosListPage() {
                 className={css.item}
                 key={photo.id}
                 onClick={(e) => {
-                  setOpenPhoto(photo)
+                  setOpenPhoto({ i })
                 }}
               >
                 <img className={css.photo} src={photo.preview} alt="" />
