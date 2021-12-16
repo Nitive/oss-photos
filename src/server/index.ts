3148153125e1c8198a28bd76c04b65711fbaa02e
Sergey Dmitriev <iserdmi@gmail.com>
2021-12-16 13:58:50 +0300

    Add metadata generation

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,7 +1,10 @@
+import 'dotenv/config'
 import * as Koa from "koa"
 import * as koaBody from "koa-body"
 import * as Router from "koa-router"
 import * as cors from "@koa/cors"
+import { CronJob } from "cron"
+import { generateMetaData, getMetaData } from "./generateMetaData"
 
 const app = new Koa()
 const router = new Router()
@@ -9,18 +12,21 @@ app.use(koaBody())
 app.use(router.routes()).use(router.allowedMethods())
 app.use(cors())
 
-router.get("/photos", (ctx, next) => {
+router.get("/photos", async (ctx: any, next: any) => {
   const { limit } = ctx.request.query
-
-  const photos = Array.from(Array(Number(limit))).map(() => ({
-    preview:
-      "https://www.natalieportman.com/wp-content/uploads/2021/08/6F9362C6-8DC9-44A2-8C17-98B8058E075F.jpeg",
-    big: "https://www.natalieportman.com/wp-content/uploads/2021/08/6F9362C6-8DC9-44A2-8C17-98B8058E075F.jpeg",
-    original:
-      "https://www.natalieportman.com/wp-content/uploads/2021/08/6F9362C6-8DC9-44A2-8C17-98B8058E075F.jpeg",
-  }))
-  ctx.body = photos
+  const metaData = await getMetaData()
+  ctx.body = metaData
   next()
 })
 
 app.listen(3000)
+
+generateMetaData()
+new CronJob(
+  "* * * * *",
+  () => {
+    generateMetaData()
+  },
+  null,
+  true
+)
